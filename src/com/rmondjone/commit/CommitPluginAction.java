39a905ea367c81f5773a1b911569390565468ea8
guohanlin <guohanlin@xyz.cn>
2019-04-16 14:32:13 +0800

    提交插件工程初始化

---
+++ src/com/rmondjone/commit/CommitPluginAction.java
@@ -0,0 +1,17 @@
+package com.rmondjone.commit;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * 注释：提交信息插件
+ * 时间：2019/4/16 0016 14:08
+ * 作者：郭翰林
+ */
+public class CommitPluginAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
